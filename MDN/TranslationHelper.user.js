mozilla-japan/translation

commit 39203ab3516f50ee342db6966b079ddc0d9faf59

name属性追加処理の正規表現を修正
間にスペースを含まずに見出しが連続する（ライブサンプルなど）場合に、次の開始タグの途中までその他属性としてキャプチャされてしまっていた。

MDN/TranslationHelper.user.js
@@ -83,7 +83,7 @@
             //this.work_str = this.work_str.replace(/<h(\d) id="(\w+)">/g, '<h$1 id="$2" name="$2">');
             let processed = 0;
             let skipped = [];
-            this.work_str = this.work_str.replace(/<h(\d) ([^ ]* *)id="([^"]+)">/g, (src, lv, other, id) => {
+            this.work_str = this.work_str.replace(/<h(\d) ([^ >]* +)?id="([^"]+)">/g, (src, lv, other, id) => {
                 if (id.match(/[^A-Za-z0-9_\-–;'\.\(\)&]/)) {
                     console.log(`addNameAttribute: skipped ${src}`);
                     skipped.push(id + ' (&lt;h' + lv + '&gt;)');

commit 4efbf072ae88876cfe8beca07f22464fe29d04be

name属性追加処理でゴミが入ってしまうのを修正
(regression in 39203ab3)

MDN/TranslationHelper.user.js
@@ -94,7 +94,7 @@
                     return src;
                 } else {
                     processed++;
-                    return `<h${lv} ${other}id="${id}" name="${id}">`;
+                    return `<h${lv} ${other || ''}id="${id}" name="${id}">`;
                 }
             });
             if (skipped.length) {